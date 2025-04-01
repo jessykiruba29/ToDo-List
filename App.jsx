e9fac61733e315e7918b3cf2a80cecee82016188
jessykiruba29 <jessykiruba2006@gmail.com>
2025-04-01 10:29:56 +0530

    Create App

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Theader from './Theader.jsx'
+import Todo from './Todo.jsx'
+
+function App() {
+  return(
+    <>
+      <Theader></Theader>
+      <Todo></Todo>
+     
+      
+    </>
+    
+  );
+  
+}
+
+export default App
